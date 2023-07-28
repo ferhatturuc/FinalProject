073df2c772b4f990cbb48a79a2ab2de5f4276293
4862348 <4862348@UBILYZLSMD017>
2023-07-28 13:38:41 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Eklendi.");
+        }
+
+        public void Delete(Product product)
+        {
+            Console.WriteLine("Silindi.");
+        }
+
+        public List<Product> GetAll()
+        {
+            Console.WriteLine("Listelendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine("Güncellendi.");
+        }
+    }
+}

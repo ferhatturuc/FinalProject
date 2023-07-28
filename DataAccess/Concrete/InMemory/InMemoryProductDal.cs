893344f09aae6cb896b8a4f5cb13181371b1911f
4862348 <4862348@UBILYZLSMD017>
2023-07-28 14:49:56 +0300

    Commit

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -10,24 +10,61 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracle,Sql Server, Postgres , MongoDb
+            _products = new List<Product> 
+            {
+                new Product {ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product {ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product {ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product {ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product {ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+            };
+        }
         public void Add(Product product)
         {
-            Console.WriteLine("Eklendi.");
+            _products.Add(product);
         }
 
         public void Delete(Product product)
         {
-            Console.WriteLine("Silindi.");
+            //Aşsağıdaki gibi newlersek ram da yeni birdeğer tutacagı ıcın bos yere pc yi yorarız
+            //Product producToDelete = new Product();
+            /*Product productToDelete = null;
+            foreach (var p in _products)
+            {
+                if (p.ProductId == productToDelete.ProductId)
+                {
+                    productToDelete = p;
+                }
+            }*/
+
+            //LINQ - Language Integrated Query
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
         }
 
         public List<Product> GetAll()
         {
-            Console.WriteLine("Listelendi");
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
         }
 
         public void Update(Product product)
         {
-            Console.WriteLine("Güncellendi.");
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
     }
 }

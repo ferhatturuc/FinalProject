4006ca22b4cd79c11c62ca14cb980f8e29571ca8
4862348 <4862348@UBILYZLSMD017>
2023-07-31 14:34:58 +0300

    Ef

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,17 +1,33 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle - yeni bir özellik eklenirken mevcut kodu değiştirmezsin
     internal class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            //InMemoryProductDal'dan EfProductDal'e gectik
+            //ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            /*foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine("Ürün Adı:" + product.ProductName + " Ürün fiyatı: " + product.UnitPrice+ " Stok Sayısı:"+ product.UnitsInStock);
+            }*/
+
+            /*foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine("Ürün Adı:" + product.ProductName + " Ürün fiyatı: " + product.UnitPrice + " Stok Sayısı:" + product.UnitsInStock);
+            }*/
+
+            foreach (var product in productManager.GetAllByUnitPrice(40, 100))
+            {
+                Console.WriteLine("Ürün Adı:" + product.ProductName + " Ürün fiyatı: " + product.UnitPrice + " Stok Sayısı:" + product.UnitsInStock);
             }
         }
     }

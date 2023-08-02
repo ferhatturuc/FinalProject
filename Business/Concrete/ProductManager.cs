3fdfff4ddfcc438dca17ab3f1f7ea452097400d8
4862348 <4862348@UBILYZLSMD017>
2023-08-02 13:36:19 +0300

    commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -40,10 +40,11 @@ namespace Business.Concrete
             //İş kodları - Yetkisi var mı ? 
             //asağıdaki gibi yazarsak iş kodlarının tamamı bellekte çalışır
             //InMemoryProductDal inMemoryProductDal = new InMemoryProductDal();
-            if (DateTime.Now.Hour == 15)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            
+            //if (DateTime.Now.Hour == 15)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
 
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
 
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
 namespace WebAPI
 {
     public class Program
@@ -7,8 +12,9 @@ namespace WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            builder.Services.AddSingleton<IProductService, ProductManager>();
+            builder.Services.AddSingleton<IProductDal,EfProductDal>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();

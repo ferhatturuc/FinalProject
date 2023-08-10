9313a46387ba5c2fdf3b08bfe929125c3b78f013
4862348 <4862348@UBILYZLSMD017>
2023-08-10 16:49:28 +0300

    CoreModule

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,11 @@
 using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using Business.DependencyResolvers.Autofac;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -30,13 +33,10 @@ namespace WebAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            //aþaðýdaki kodla autofac i kullanýyoruz
-            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-            builder.Host.ConfigureContainer<ContainerBuilder>(options =>
-            {
-                options.RegisterModule(new AutofacBusinessModule());
-            });
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //ServiceTool.Create(services);
 
+            //builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -53,6 +53,22 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+
+            builder.Services.AddDependencyResolvers(new ICoreModule[]
+            {
+                new CoreModule()
+            });
+
+            //ServiceTool.Create(builder.Services);
+
+            //aþaðýdaki kodla autofac i kullanýyoruz
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer<ContainerBuilder>(options =>
+            {
+                options.RegisterModule(new AutofacBusinessModule());
+            });
+
+
 
             var app = builder.Build();
 

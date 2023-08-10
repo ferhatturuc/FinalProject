746c7726840f8649ef5a203766e88b464390bc92
4862348 <4862348@UBILYZLSMD017>
2023-08-10 10:57:46 +0300

    AuthManager

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -16,5 +16,12 @@ namespace Business.Constants
         public static string ProductNameAlreadyExist = "Bu isimde bir ürün zaten var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
         public static string AuthorizationDenied = "Yetkiniz Yok";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
